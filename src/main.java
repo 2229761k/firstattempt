e513b5534b7e6529f4a446adbef40c059396a467
John Jang <jjohnjjang@gmail.com>
2017-09-17 13:16:20 -0700

    change version 3

--- src/main.java
+++ src/main.java
@@ -2,31 +2,39 @@ import java.util.*;
 
 public class main {
 
+    int[][] map = new int[3][3];
+
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		String a;
-		
+
+        printMatrix(askUserInterface());
+
+
+	}
+
+    public int[][] askUserInterface() {
+    	String n;
 		Scanner sc = new Scanner(System.in);
 		System.out.println("which position?");
 		while(!sc.hasNext()){
 			sc.next();
 			System.out.println("out of position, try again");
 		}
-		a=sc.nextLine();
-		String b[]=a.split(",");
-		System.out.println(b[0]);
-		System.out.println(b[1]);
-		//System.out.println(askUserInterface());
+		String[] rawInput =sc.nextLine().split(",");
+        int[][] intInput = new int[3][3];
+        return intInput[rawInput[0]][rawInput[1]] = 1;
 
-	}
-
-   // public String askUserInterface() {
-   // 	String n;
+    }
     	
     	 
-    	
-	//	return ;
-        // Ask user for an input and return the input
+    public void printMatrix(int matrix[][]) {
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
+                System.out.print(matrix[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
 
 
     
