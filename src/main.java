ef2723da8802d1e03eb6b9909dba785c9c63bbc6
John Jang <jjohnjjang@gmail.com>
2017-09-17 12:31:50 -0700

    task1

--- src/main.java
+++ src/main.java
@@ -4,6 +4,15 @@ public class main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+        //System.out.println(askUserInterface())
+
 	}
+
+    public int askUserInterface() {
+        // Ask user for an input and return the input
+
+
+    }
+
 
 }
