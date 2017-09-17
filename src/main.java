75170cc9ce539243fe9cf440a719238d680947d9
Dohyung Kim <doblyrubly@Dohyungs-MacBook-Air.local>
2017-09-17 21:05:47 +0100

    change2

--- src/main.java
+++ src/main.java
@@ -1,18 +1,35 @@
+import java.util.*;
 
 public class main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
-        //System.out.println(askUserInterface())
+		String a;
+		
+		Scanner sc = new Scanner(System.in);
+		System.out.println("which position?");
+		while(!sc.hasNext()){
+			sc.next();
+			System.out.println("out of position, try again");
+		}
+		a=sc.nextLine();
+		String b[]=a.split(",");
+		System.out.println(b[0]);
+		System.out.println(b[1]);
+		//System.out.println(askUserInterface());
 
 	}
 
-    public int askUserInterface() {
+   // public String askUserInterface() {
+   // 	String n;
+    	
+    	 
+    	
+	//	return ;
         // Ask user for an input and return the input
 
 
-    }
+    
 
 
 }
